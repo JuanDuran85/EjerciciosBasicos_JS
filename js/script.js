44c099782e150f3b9658270626d6d4596d9ae431
JuanDuran85 <duran85juan@gmail.com>
2020-02-13 09:48:28 -0300

    inicio repositorio de JS Ejercicios Basicos

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/*-------------------------------------------------------
+            Ejercicio Básico - Primera Clase
+--------------------------------------------------------*/
+const PI = 3.141516;
+
+var num1 = 40;
+var num2 = 20;
+
+var resultado = num1 + num2 + PI;
+
+console.log(resultado);
+console.log(PI);
+
+document.write("La constante PI es igual a: "+PI);
+
+document.write("<p>El resultado es: "+resultado+ " de la sumatoria de los numeros "+num1+ " y "+num2+"</p>");
+
+var nombre = prompt("Ingrese su nombre","Ejemplo: Juan");
+alert("El nombre ingresado fue: "+nombre);
+
+var a = "hola2";
+
+if (true) {
+    let a = "hola";
+    console.log(a);
+}
+
+/*-------------------------------------------------------
+            ------------------------------------
+--------------------------------------------------------*/
