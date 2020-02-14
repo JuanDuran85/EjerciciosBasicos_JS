daca6b7407058df30eb3ba8707783d4543bc9d51
JuanDuran85 <duran85juan@gmail.com>
2020-02-14 12:38:30 -0300

    modificando el ejercicio 6 - formulario validado basico

--- js/script6.js
+++ js/script6.js
@@ -2,14 +2,27 @@
     var formulario = document.getElementById('formulario');
     var elementos = formulario.elements;
     var boton = document.getElementById('boton');
-    console.log(formulario);
-    console.log(elementos);
-    console.log(boton);
-    
+    var i=0;
+
     var validando = function (e) {
+                
         if (formulario.nombre.value == 0) {
-            alert("No ingreso un valor");
+            alert("No ingreso un valor en el Nombre");
             e.preventDefault();
+            i++;
+        }
+        
+        if (formulario.correo.value == 0) {
+            alert("No ingreso un valor en el Correo");
+            e.preventDefault();
+            i++;
+        }
+
+        if (i  > 0) {
+            i=0;
+            alert("No se puede enviar el Formulario");
+        }else {
+            alert("Formulario enviado");
         }
     };
 
