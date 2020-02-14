c36cf6dc9253c2d54fa79944cbe61af5dacdd217
JuanDuran85 <duran85juan@gmail.com>
2020-02-14 10:22:46 -0300

    modificando ejercicio 6 con formulario y validacion simple

--- js/script6.js
+++ js/script6.js
@@ -1,7 +1,18 @@
 (function () {  
     var formulario = document.getElementById('formulario');
-    var elementros = formulario.elements;
+    var elementos = formulario.elements;
     var boton = document.getElementById('boton');
+    console.log(formulario);
+    console.log(elementos);
+    console.log(boton);
+    
+    var validando = function (e) {
+        if (formulario.nombre.value == 0) {
+            alert("No ingreso un valor");
+            e.preventDefault();
+        }
+    };
 
-    formulario.addEventListener('submit', validando());
+    formulario.addEventListener("submit", validando);
+
 }());
