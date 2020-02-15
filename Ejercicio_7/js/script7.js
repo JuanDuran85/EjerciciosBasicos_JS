ee983c76c547b213b81a9cd553e536e4f2458090
JuanDuran85 <duran85juan@gmail.com>
2020-02-14 21:52:29 -0300

    agregando JS al ejercicio 7

---
+++ Ejercicio_7/js/script7.js
@@ -0,0 +1,10 @@
+function validando() {
+    var nombre, apellido, correo, usuario, clave, telefono;
+
+    nombre = document.getElementById("nombre").value;
+    apellido = document.getElementById("apellido").value;
+    correo = document.getElementById("correo").value;
+    usuario = document.getElementById("usuario").value;
+    clave = document.getElementById("clve").value;
+    telefono = document.getElementById("telefono").value;
+}
