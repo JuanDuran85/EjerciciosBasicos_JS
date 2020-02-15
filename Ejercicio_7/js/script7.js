434b18f70cb63a3ffcbee9a360466a5cc303d25b
JuanDuran85 <duran85juan@gmail.com>
2020-02-15 00:25:19 -0300

    agregando validaciones con JS al ejercicio 7

--- Ejercicio_7/js/script7.js
+++ Ejercicio_7/js/script7.js
@@ -14,7 +14,47 @@ function validando() {
             title: 'Oops...',
             text: 'Debes ingresar valores validos',
             footer: 'No puedes dejar las casillas sin valores'
-          })
+          });
           return false;
-    };
+    } else if (nombre.length > 20) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Debes ingresar un nombre valido',
+            footer: 'El máximo permitido es de 20 caracteres de largo'
+          });
+          return false;
+    } else if (apellido.length > 50) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Debes ingresar un apellido valido',
+            footer: 'El máximo permitido es de 50 caracteres de largo'
+          });
+          return false;
+    } else if (correo.length > 100) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Debes ingresar un correo electrónico valido',
+            footer: 'El máximo permitido es de 100 caracteres de largo'
+          });
+          return false;
+    } else if (usuario.length > 20 || clave.length > 20 ) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Debes ingresar un usuario y/o contraseña valida',
+            footer: 'El máximo permitido es de 20 caracteres de largo para ambos parametros'
+          });
+          return false;
+    } else if (telefono.length > 11) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Debes ingresar un número telefónico valido',
+            footer: 'El máximo permitido es de 11 caracteres de largo'
+          });
+          return false;
+    }
 }
