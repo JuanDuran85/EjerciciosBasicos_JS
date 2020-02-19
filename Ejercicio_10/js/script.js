aaa037ce5975de3288002604d013bfda4e65400d
JuanDuran85 <duran85juan@gmail.com>
2020-02-19 17:28:51 -0300

    mejorando validacion

--- Ejercicio_10/js/script.js
+++ Ejercicio_10/js/script.js
@@ -1,20 +1,24 @@
 window.onload = inicio;
-console.log("entro");
+
 function inicio () {
-    console.log("inicio");
+
     document.getElementById("enviar").addEventListener('click', validando);
 }
 
 function validar_nombre() {
-    console.log("nombre");
+
     var nombre = document.getElementById("nombre");
     borrar_error(nombre);
     if (!nombre.checkValidity()) {
-        console.log("entro malo");
-        error(nombre);
+        if (nombre.validity.valueMissing) {
+            error2(nombre,"Debe ingresar un nombre");
+        }
+        if (nombre.validity.patternMismatch) {
+            error2(nombre,"Debe ingresar un nombre valido entre 2 y 30 caracteres");
+        }
+        //error(nombre);
         return false;
     };
-    console.log("nombre bueno");
     return true;
 }
 
@@ -22,7 +26,16 @@ function validar_edad() {
     var edad = document.getElementById("edad");
     borrar_error(edad);
     if (!edad.checkValidity()) {
-        error(edad);
+        if(edad.validity.valueMissing) {
+            error2(nombre,"Debe ingresar una edad");
+        }
+        if (edad.validity.rangeOverflow) {
+            error2(nombre,"el valor debe ser menor a 100");
+        }
+        if (edad.validity.rangeUnderflow) {
+            error2(nombre,"el valor debe ser mayor o igual a 18");
+        }
+        //error(edad);
         return false;
     };
     return true;
@@ -40,24 +53,27 @@ function validar_telefono() {
 
 function validando (e) {  
     if (validar_nombre() && validar_edad() && validar_telefono() && confirm("Clic para enviar")) {
-        console.log("todo bien");
         return true;
     } else {
         e.preventDefault();
-        console.log("todo mal");
         return false;
     }
 }
 
 function error(elemento) {
-    console.log("error de validacion");
     document.getElementById("muestraError").innerHTML = elemento.validationMessage;
     elemento.className = "error";
     elemento.focus();
 }
 
+function error2(elemento,mensaje) {
+    document.getElementById("muestraError").innerHTML = mensaje;
+    elemento.className = "error";
+    elemento.focus();
+}
+
+
 function borrar_error () {
-    console.log("borrar mensaje");
     var formulario = document.forms[0];
     for (let index = 0; index < formulario.elements.length; index++) {
         formulario.elements[index].className = "";
